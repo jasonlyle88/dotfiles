437fa1115d3870940b8f48c65ddcab55ff5c5365
Jason Lyle <jason.lyle88@gmail.com>
2026-05-14 13:44:30 -0400

    Make starship and mise auto-install

--- dot_config/auto_source/100_starship.zsh
+++ dot_config/auto_source/100_starship.zsh
@@ -1,7 +1,18 @@
 # shellcheck shell=bash
+
 ################################################################################
 # Setup starship
 ################################################################################
+
+# Attempt to install starship
+if ! command -v starship 1>/dev/null 2>&1; then
+    printf '################################################################################\n'
+    printf '# Install starship\n'
+    printf '################################################################################\n'
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "${HOME}/.local/bin"
+fi
+
+# Exit if starship still not available
 if ! command -v starship 1>/dev/null 2>&1; then
     return 0
 fi
--- dot_config/auto_source/500_mise.zsh
+++ dot_config/auto_source/500_mise.zsh
@@ -1,7 +1,18 @@
 # shellcheck shell=bash
+
 ################################################################################
 # Setup mise
 ################################################################################
+
+# Attempt to install mise
+if ! command -v mise 1>/dev/null 2>&1; then
+    printf '################################################################################\n'
+    printf '# Install mise\n'
+    printf '################################################################################\n'
+    curl -sS https://mise.run | sh
+fi
+
+# Exit if mise still not available
 if ! command -v mise 1>/dev/null 2>&1; then
     return 0
 fi
