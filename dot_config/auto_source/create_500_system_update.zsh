5e56438f4152c3c840db8e564dc918a4b461b835
Jason Lyle <jason.lyle88@gmail.com>
2026-05-27 06:28:52 -0400

    Add .config/auto_source/500_system_update.zsh

---
+++ dot_config/auto_source/create_500_system_update.zsh
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+
+################################################################################
+# Setup symlink helpers
+################################################################################
+
+export SYSTEM_UPDATE_INSTALL_SQLCL=0
+export SYSTEM_UPDATE_INSTALL_LIQUIDBASE=0
+export SYSTEM_UPDATE_INSTALL_JAVA=0
+export SYSTEM_UPDATE_INSTALL_NODE=0
+export SYSTEM_UPDATE_INSTALL_PYTHON=0
