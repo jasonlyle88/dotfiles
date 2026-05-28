5d2ff6efb9a9a54f7e7ba9ede0915b9b211b5a61
Jason Lyle <jason.lyle88@gmail.com>
2026-05-28 07:47:36 -0400

    Update .config/auto_load/system.update Update .config/auto_source/500_mise.zsh

--- dot_config/auto_source/500_mise.zsh
+++ dot_config/auto_source/500_mise.zsh
@@ -17,8 +17,6 @@ if ! command -v mise 1>/dev/null 2>&1; then
     return 0
 fi
 
-export MISE_DEFAULT_CONFIG_FILENAME="${XDG_CONFIG_HOME}/mise/config-global.toml"
-
 # Activate mise
 eval "$(mise activate zsh)"
 
@@ -26,3 +24,47 @@ eval "$(mise activate zsh)"
 if [[ ! -f "${XDG_DATA_HOME}/zsh/site-functions/_mise" ]]; then
     mise completion zsh  > "${XDG_DATA_HOME}/zsh/site-functions/_mise"
 fi
+
+# Wrap mise so global set/unset/use/unuse writes to the chezmoi-managed file.
+mise() {
+    local env_config_file="${XDG_CONFIG_HOME}/mise/conf.d/500-env.toml"
+    local tool_config_file="${XDG_CONFIG_HOME}/mise/conf.d/500-tools.toml"
+
+    case "$1" in
+        set|unset)
+            _mise_rewrite_global_flag "--file" "${env_config_file}" "$@"
+            ;;
+
+        use|unuse)
+            _mise_rewrite_global_flag "--path" "${tool_config_file}" "$@"
+            ;;
+
+        *)
+            command mise "$@"
+            ;;
+    esac
+}
+
+_mise_rewrite_global_flag() {
+    local replacement_flag="$1"
+    local replacement_arg="$2"
+    shift 2
+
+    local -a args
+    local arg
+    local replaced=0
+
+    for arg in "$@"; do
+        case "${arg}" in
+            -g|--global)
+                args+=("${replacement_flag}" "${replacement_arg}")
+                replaced=1
+                ;;
+            *)
+                args+=("${arg}")
+                ;;
+        esac
+    done
+
+    command mise "${args[@]}"
+}
