a3e794cf1ceb05e410135886849a0034ab5589d6
Jason Lyle <jason.lyle88@gmail.com>
2026-06-06 10:09:42 -0400

    Update .config/auto_source/500_zoxide.zsh Update .ssh/known_hosts

--- dot_config/auto_source/500_zoxide.zsh
+++ dot_config/auto_source/500_zoxide.zsh
@@ -12,6 +12,12 @@ fi
 # Initialize zoxide for ZSH
 eval "$(zoxide init zsh)"
 
+# Store what zoxide uses as a default when nothing is set in _ZO_FZF_OPTS
+export _ZO_FZF_OPTS_DEFAULT_BASE='--exact --no-sort --bind=ctrl-z:ignore,btab:up,tab:down --cycle --keep-right --border=sharp --height=45% --info=inline --layout=reverse --tabstop=1 --exit-0'
+
+# Set _ZO_FZF_OPTS with the default options and my own options
+export _ZO_FZF_OPTS="${_ZO_FZF_OPTS_DEFAULT_BASE} --select-1"
+
 # Set up aliases for easier navigation
 alias cd='z'
 alias cdi='zi'
