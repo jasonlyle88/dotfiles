75ba517a7caf75553fc7c3a7fa9bed13a471ff47
Jason Lyle <jason.lyle88@gmail.com>
2026-06-18 09:35:21 -0400

    Update .config/raycast/custom-scripts/oracle-sso-login.sh

--- dot_config/raycast/custom-scripts/executable_oracle-sso-login.sh
+++ dot_config/raycast/custom-scripts/executable_oracle-sso-login.sh
@@ -13,6 +13,37 @@
 # @raycast.description Oracle SSO login
 # @raycast.author Jason Lyle
 
-open -n 'https://signon-int-stage.oracle.com/'
+# open -n 'https://signon-int-stage.oracle.com/'
 # sleep 0.5
 # open 'https://signon-int.oracle.com/'
+
+url='https://signon-int.oracle.com/'
+bundle_id="$(duti -d https)"
+
+case "${bundle_id}" in
+    com.google.Chrome|com.google.Chrome.canary|com.brave.Browser|com.microsoft.edgemac)
+        osascript \
+            -e "tell application id \"${bundle_id}\"" \
+            -e "make new window" \
+            -e "set URL of active tab of front window to \"${url}\"" \
+            -e "activate" \
+            -e "end tell"
+        ;;
+
+    org.mozilla.firefox)
+        /Applications/Firefox.app/Contents/MacOS/firefox --new-window "${url}"
+        ;;
+
+    com.apple.Safari)
+        osascript \
+            -e "tell application id \"com.apple.Safari\"" \
+            -e "make new document with properties {URL:\"${url}\"}" \
+            -e "activate" \
+            -e "end tell"
+        ;;
+
+    *)
+        # Fallback: opens in default browser, but may be a tab
+        open "${url}"
+        ;;
+esac
