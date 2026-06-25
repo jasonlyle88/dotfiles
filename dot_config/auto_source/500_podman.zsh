76ed4a3b5e8463ac4cb3aa75c4a94b672d0a0ed1
Jason Lyle <jason.lyle88@gmail.com>
2026-06-25 07:04:31 -0400

    Add .config/auto_source/500_podman.zsh

---
+++ dot_config/auto_source/500_podman.zsh
@@ -0,0 +1,23 @@
+# shellcheck shell=bash
+
+################################################################################
+# Setup Python
+################################################################################
+
+# On linux machines, make sure the user podman socket is enabled:
+# systemctl --user enable --now podman.socket
+
+if ! command -v podman 1>/dev/null 2>&1; then
+    return 0
+fi
+
+# Setup DOCKER_HOST to point to the podman socket
+# This allows tools like docker-compose to work with podman
+DOCKER_HOST_VALUE="$(podman info --format '{{.Host.RemoteSocket.Path}}')"
+if [[ "${DOCKER_HOST_VALUE}" != 'unix://'* ]]; then
+    DOCKER_HOST_VALUE="unix://${DOCKER_HOST_VALUE}"
+fi
+export DOCKER_HOST="${DOCKER_HOST_VALUE}"
+
+# do not show warning message that `podman compose` is executing an external binary
+# export PODMAN_COMPOSE_WARNING_LOGS=false
